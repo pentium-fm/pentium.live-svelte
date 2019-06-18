97df68918a0de1fd006e6118d33b2619b9695a7f
Abderrhim Soubai <gx.coder@gmail.com>
2019-06-18 22:19:14 +0100

    Add title to the page + tiny fixes

--- src/Current.svelte
+++ src/Current.svelte
@@ -6,20 +6,37 @@
 
 <style>
 
-
-
-span {
-
+.played {
     padding : 10px;
+    margin-left: 5px;
+}
+
+
+.play {
+  position: relative;
+  margin-right: 15px;
+  height: 45px;
+  width: 45px;
+  border-radius: 100%;
+}
+.play:after {
+  content: '';
+  position: absolute;
+  background: white;
+  height: 20px;
+  width: 25px;
+  clip-path: polygon(62% 54%, 0 0, 0 100%);
+  top: 0px;
+  left: 0px;
 }
 
 
 
 </style>
 
+
 {#if current }
-
-<span class="play"></span> Playing : {current.title}
-
-
+<div class="played"> 
+        <i class="play"></i>	&nbsp;	&nbsp;	&nbsp;Playing : {current.title}
+</div>
 {/if}
